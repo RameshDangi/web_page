7dd823a638bb5e07b92da39e9988efaf157013e2
Ramesh <rameshdangi734@gmail.com>
2025-08-03 01:01:20 +0545

    make exit() functon after redirect

--- insert.php
+++ insert.php
@@ -6,16 +6,19 @@ if (isset($_POST['submit'])) {
     $cpwd = $_POST['cpwd'];
 
     if($pwd == $cpwd) {
+        $hashedpwd = sha1($pwd);
         include './connection.php';
         $sql = "INSERT INTO users (email, address, password)
-        VALUES ('$email', '$addr', '$pwd')";
+        VALUES ('$email', '$addr', '$hashedpwd')";
         $res = mysqli_query($conn, $sql);
         if($res) {
             $_SESSION['msg'] = "User registration successfull";
             header("location: ./login.php");
+            exit();
         }else {
             $_SESSION['msg'] = "User registration failed";
             header("location: ./sign_up.php");
+            exit();
         }
     } else {
         $_SESSION['msg'] = "Password not matched";
